f190ddbebbcea3a25108cfe59116c3aee9165baa
Conradk10 <orion25rus@gmail.com>
2022-04-27 19:44:38 +0300

    Апдейт логов

---
+++ handlers/user/rmkb.py
@@ -0,0 +1,16 @@
+import keyboards.default
+from loader import dp
+from aiogram import types, html
+from utils.misc_funcs import get_greetings
+
+
+@dp.message(commands=["start"])
+async def cmd_start(message: types.Message, user_data: dict):
+    text = get_start_text(user_data)
+    kb = keyboards.default.main_menu.MainMenu.main_menu()
+    await message.answer(text, reply_markup=kb)
+
+
+def get_start_text(user_data: dict):
+    text = f"{get_greetings().capitalize()}, {html.bold(html.quote(user_data['username']))}!"
+    return text
